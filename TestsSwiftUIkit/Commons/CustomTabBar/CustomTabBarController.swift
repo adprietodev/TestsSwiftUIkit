779ff4f5031c7443f38360b8ea315896e88fbd14
adprietodev <adrianwork95@outlook.com>
2024-06-06 17:16:58 +0200

    Add folders Add CustomTabBar

---
+++ TestsSwiftUIkit/Commons/CustomTabBar/CustomTabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTabBar.swift
+//  TestsSwiftUIkit
+//
+//  Created by Adrian Prieto Villena on 6/6/24.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    func build() -> CustomTabBarController {
+        self
+    }
+
+    private func setupUI() {
+        
+    }
+}
